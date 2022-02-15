15cdb69b9677095f3e322830880eb41084e2f356
Suraksha <surakshamotilal@gmail.com>
2022-02-15 10:02:36 +0200

    Add files via upload

--- Question1_Calendar.py
+++ Question1_Calendar.py
@@ -1,12 +1,5 @@
 #QUESTION 1: GENERATED CALENDAR
 
-class color:
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
-
 #array to store the days of the week
 DaysOfWeek=["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
 
@@ -14,42 +7,47 @@ DaysOfWeek=["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
 startDay=int(input()); #day of week that month starts on
 numDays=int(input()); #num days in month
 
-for i in range(0,len(DaysOfWeek)):
-    print(DaysOfWeek[i], end ="");
-    #ensure there isn't a tab at the end
-    if (i<len(DaysOfWeek)-1):
-        print("\t", end ="");
+#check that input is valid
+if (startDay>7 or startDay<0 or numDays<28 or numDays>31):
+    print("Invalid")
     
-print("\n", end ="")
-#stores number of days
-count=1;
-#only output first week 
-for i in range(0,7):
-    if (i<startDay-1):
-        print("\t", end =" ");
-    else:
-        print(count, end ="");
-        if (i!=6):
+else:
+    for i in range(0,len(DaysOfWeek)):
+        print(DaysOfWeek[i], end ="");
+        #ensure there isn't a tab at the end
+        if (i<len(DaysOfWeek)-1):
             print("\t", end ="");
-        count+=1;
-print("\n", end ="")
-weekCounter=0;
-while (count<=numDays):
-    #if it is valentine's day
-    if (count==14 and (numDays==28 or numDays==29)):
-        print(color.RED + str(count) + color.END,end ="")
-    else:
-        print(count, end ="");
-    count+=1;
-    if (weekCounter!=6):
-        print("\t", end ="");
         
-    weekCounter+=1;
-    #ensures new weeks are printed on new lines
-    if (weekCounter==7):
-        weekCounter=0;
-        print("\n",end="");
+    print("\n", end ="")
+    #stores number of days
+    count=1;
+    #only output first week 
+    for i in range(0,7):
+        if (i<startDay-1):
+            print("\t", end =" ");
+        else:
+            print(count, end ="");
+            if (i!=6):
+                print("\t", end ="");
+            count+=1;
+    print("\n", end ="")
+    weekCounter=0;
+    while (count<=numDays):
+        #if it is valentine's day
+        if (count==14 and (numDays==28 or numDays==29)):
+            print("X",end ="")
+        else:
+            print(count, end ="");
+        count+=1;
+        if (weekCounter!=6):
+            print("\t", end ="");
+            
+        weekCounter+=1;
+        #ensures new weeks are printed on new lines
+        if (weekCounter==7):
+            weekCounter=0;
+            print("\n",end="");
+        
     
-
-    
+        
 
